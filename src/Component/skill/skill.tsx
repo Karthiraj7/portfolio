5239882487c1949d271114ef764dde8ca9b3c370
unknown <k.r.karthiraj.mtp.06@gmail.com>
2025-07-25 04:04:35 +0530

    Made CSS responsive for all devic

--- src/Component/skill/skill.tsx
+++ src/Component/skill/skill.tsx
@@ -1,4 +1,14 @@
 import './skill.css';
+import HTML from '../../assets/icon/html.png';
+import CSS from '../../assets/icon/css.png'
+import JAVASCRIPT from '../../assets/icon/javascript.jpg'
+import TYPESCRIPT from '../../assets/icon/typescript.png';
+import NODEJS from '../../assets/icon/nodejs.png'
+import JQUERY from '../../assets/icon/jquery.png'
+import SHAREPOINT from '../../assets/icon/sharepoint.webp'
+import POWERAPPS from '../../assets/icon/Power-Apps-1.png'
+import GIT from '../../assets/icon/Git-Icon-1788C 1.png'
+import REACT from '../../assets/icon/react.jpg'
 
 const Skills = () => {
   return (
@@ -9,37 +19,46 @@ const Skills = () => {
         <h3>USING NOW:</h3>
         <div className="skills-grid">
           <div className="skill-item">
-            <img src="/images/html5.png" alt="HTML5" />
+            <img src={HTML} alt="HTML5" />
             <p>HTML5</p>
           </div>
           <div className="skill-item">
-            <img src="/images/css3.png" alt="CSS3" />
+            <img src={CSS} alt="CSS3" />
             <p>CSS3</p>
           </div>
           <div className="skill-item">
-            <img src="/images/sass.png" alt="SASS" />
-            <p>SASS</p>
-          </div>
-          <div className="skill-item">
-            <img src="/images/js.png" alt="JavaScript" />
+            <img src={JAVASCRIPT} alt="SASS" />
             <p>JAVASCRIPT</p>
           </div>
           <div className="skill-item">
-            <img src="/images/react.png" alt="React" />
+            <img src={TYPESCRIPT} alt="JavaScript" />
+            <p>TYPESCRIPT</p>
+          </div>
+          <div className="skill-item">
+            <img src={NODEJS} alt="React" />
+            <p>NODEJS</p>
+          </div>
+          <div className="skill-item">
+            <img src={JQUERY} alt="Bootstrap" />
+            <p>JQUERY</p>
+          </div>
+          <div className="skill-item">
+            <img src={REACT} alt="Git" />
             <p>REACT</p>
           </div>
-          <div className="skill-item">
-            <img src="/images/bootstrap.png" alt="Bootstrap" />
-            <p>BOOTSTRAP</p>
+           <div className="skill-item">
+            <img src={SHAREPOINT} alt="Bootstrap" />
+            <p>SHAREPOINT</p>
+          </div>
+           <div className="skill-item">
+            <img src={POWERAPPS} alt="Bootstrap" />
+            <p>POWERAPPS</p>
           </div>
           <div className="skill-item">
-            <img src="/images/git.png" alt="Git" />
+            <img src={GIT} alt="Git" />
             <p>GIT</p>
           </div>
-          <div className="skill-item">
-            <img src="/images/figma.png" alt="Figma" />
-            <p>FIGMA</p>
-          </div>
+         
         </div>
       </div>
 
